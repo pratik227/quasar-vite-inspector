b26bc8f0fd5e4e68a3d3ab583f9f0153be0209d4
pratikpatel <pratikpatelpp802@gmail.com>
2023-02-03 16:14:38 +0530

    Initial commit

---
+++ src/components/Test2.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-card class="my-card">
+    <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+    <q-card-section>
+      <div class="text-h6">Our Changing Planet</div>
+      <div class="text-subtitle2">by John Doe</div>
+    </q-card-section>
+
+    <q-card-section class="q-pt-none">
+      {{ lorem }}
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: "Test2",
+  data() {
+    return {
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="q-pa-sm">
+    <div class="row q-col-gutter-md">
+      <div class="col-6">
+        <test-component></test-component>
+      </div>
+      <div class="col-6">
+        <test2></test2>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import {defineComponent, defineAsyncComponent} from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: {
+    TestComponent: defineAsyncComponent(() => import("components/TestComponent.vue")),
+    Test2: defineAsyncComponent(() => import("components/Test2.vue"))
+  }
+})
+</script>
